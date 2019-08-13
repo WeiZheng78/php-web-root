2f8b8dad1930c0f6efb46661dd1a8f1016fcb1b3
WeiZheng78 <weizheng78@gmail.com>
2019-08-13 14:46:28 +0800

    Update config.php

--- admin/config.php
+++ admin/config.php
@@ -4,12 +4,19 @@
  * Configuration for database connection
  *
  */
+$MYSQL_PORT = getenv('MYSQL_PORT', true) ?: getenv('MYSQL_PORT');
+$HOSTPORT_ARRAY_0  = explode("//", getenv('MYSQL_PORT'));
+$HOSTPORT_ARRAY_1  = explode(":", $HOSTPORT_ARRAY_0[1]);
+$host       = $HOSTPORT_ARRAY_1[0];
+$port       = $HOSTPORT_ARRAY_1[1];
 
-$host       = getenv('MYSQL_HOST');
-$username   = getenv('MYSQL_USER');
-$password   = getenv('MYSQL_PASSWORD');
-$dbname     = getenv('MYSQL_DBNAME');
-$dsn        = "mysql:host=$host;dbname=$dbname";
+$dbname     = "application";
+$dbname     = getenv('MYSQL_DBNAME', true) ?: getenv("MYSQL_DBNAME");
+$dsn        = "mysql:host=$host;port=$port;dbname=$dbname";
 $options    = array(
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
               );
+
+$username   = getenv('MYSQL_USER', true) ?: getenv('MYSQL_USER');
+$password   = getenv('MYSQL_PASSWORD', true) ?: getenv('MYSQL_PASSWORD');
+
