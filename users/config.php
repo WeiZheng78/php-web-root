a87c02bcc0604e47bddfd7087e7bb57a8d8c3d41
WeiZheng78 <weizheng78@gmail.com>
2019-08-13 18:01:03 +0800

    Update config.php

--- users/config.php
+++ users/config.php
@@ -10,7 +10,6 @@ $HOSTPORT_ARRAY_0  = explode("//", getenv('MYSQL_PORT'));
 $HOSTPORT_ARRAY_1  = explode(":", $HOSTPORT_ARRAY_0[1]);
 $host       = $HOSTPORT_ARRAY_1[0];
 $port       = $HOSTPORT_ARRAY_1[1];
-$dbname     = "application";
 $dbname     = getenv('MYSQL_DBNAME', true) ?: getenv("MYSQL_DBNAME");
 $dsn        = "mysql:host=$host;port=$port;dbname=$dbname";
 $options    = array(
